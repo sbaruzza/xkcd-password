6f719962f9484f4fe8d46013cc0a4b103d9db851
Simone Baruzza <simone.baruzza@gmail.com>
2014-04-28 14:31:05 +0200

    enh: error alert

--- js/password.js
+++ js/password.js
@@ -1,7 +1,7 @@
 var xkcd = {} // Will transform into a namespace declaration
 xkcd.available = [
 	{name: 'english-common-5k', description:'English common words (5K)'},
-	{name: 'english-nouns-55k', description:'English nouns (55K)'}
+	{name: 'english-nouns-55kx', description:'English nouns (55K)'}
 ];
 xkcd.selected = "english-common-5k";
 
@@ -31,12 +31,14 @@ xkcd.loadWordsList = function(name) {
 		$('#words-list-button').text(title);
 		console.log('Loaded ' + title + ' with ' + data.length + ' words.');
 	}).fail(function(){
+		$('#load-error').removeClass('hidden');
 		console.log('Failed to load words list.', arguments);
 	});
 }
 
 $(document).ready(function(){
 	$('#generate').click(xkcd.showPassword);
+	$('#load-error-close').click(function(){$('#load-error').addClass('hidden');});
 	xkcd.available.forEach(function(menu){
 		var a = document.createElement("a");
 		a.setAttribute("href", "#");
